dc2d73e15c9f39e4b50f2ca34a279224c623825f
Bhargav1112 <bhargavbhakhar1112@gmail.com>
2022-06-22 15:40:53 +0530

    local storage data

--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -1,100 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import { useFormik } from "formik";
 
 function Login(props) {
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: "",
-        },
-        validationSchema: props.schema,
-        onSubmit: (values, actions) => {
-            alert(JSON.stringify(values, null, 2));
-            actions.resetForm();
-        },
-    });
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const { handleBlur, handleChange, errors, handleSubmit, touched, values } =
-        formik;
-
-    return (
-        <div className="container">
-            <div className="row justify-content-center align-items-center">
-                <div className="col-10 col-md-6">
-                    <div className="login-form rounded p-5 border border-1">
-
-                        <Form onSubmit={handleSubmit}>
-                            <FormGroup>
-                                <Label for="email">Email</Label>
-                                <Input
-                                    value={values.email}
-                                    id="email"
-                                    name="email"
-                                    placeholder="abc@abc.com"
-                                    type="text"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                />
-                                {errors.email && touched.email && (
-                                    <p className="text-danger">
-                                        {errors.email}
-                                    </p>
-                                )}
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="Password">Password</Label>
-                                <Input
-                                    value={values.password}
-                                    id="Password"
-                                    name="password"
-                                    placeholder="password"
-                                    type="password"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                />
-                                {errors.password && touched.password && (
-                                    <p className="text-danger">
-                                        {errors.password}
-                                    </p>
-                                )}
-                            </FormGroup>
-                            <div className="mb-3 d-flex justify-content-between">
-                                <FormGroup check>
-                                    <Input type="checkbox" />
-                                    <Label check>Remember Me</Label>
-                                </FormGroup>
-                                <a
-                                    href="#"
-                                    className="link"
-                                    onClick={props.onForgotPwd}
-                                >
-                                    Forgot password?
-                                </a>
-                            </div>
-                            <Button
-                                className="w-100 mb-3 btn-dark"
-                                type="submit"
-                            >
-                                Login
-                            </Button>
-
-                            <div className="d-flex align-items-center gap-3 justify-content-center">
-                                <span>Create a new Account: </span>
-                                <Button
-                                    className="btn-warning"
-                                    onClick={props.onSignUp}
-                                >
-                                    SignUp
-                                </Button>
-                            </div>
-                        </Form>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+  const userLoginHandler = (data) => {
+    const storageData = JSON.parse(localStorage.getItem("user"));
+    const existingUserIndex = storageData.findIndex(
+      (user) => user.email === data.email && user.password === data.password
     );
+    const existingUser = storageData[existingUserIndex];
+
+    if (!existingUser) {
+      // failure state
+      setLoggedIn(false);
+    }
+    // success state
+    setLoggedIn(true);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: props.schema,
+    onSubmit: (values, actions) => {
+      userLoginHandler(values);
+      actions.resetForm();
+    },
+  });
+
+  const { handleBlur, handleChange, errors, handleSubmit, touched, values } =
+    formik;
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-10 col-md-6">
+          <div className="login-form rounded p-5 border border-1">
+            {loggedIn && (
+              <p style={{ color: "green", fonSize: "48px", margin: 0 }}>
+                Successfully Logged In...!
+              </p>
+            )}
+            {!loggedIn && touched.email && touched.password && (
+              <p style={{ color: "red", fonSize: "48px", margin: 0 }}>
+                Incorrect Email or Password...!
+              </p>
+            )}
+            {!loggedIn && (
+              <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                  <Label for="email">Email</Label>
+                  <Input
+                    value={values.email}
+                    id="email"
+                    name="email"
+                    placeholder="abc@abc.com"
+                    type="text"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.email && touched.email && (
+                    <p className="text-danger">{errors.email}</p>
+                  )}
+                </FormGroup>
+                <FormGroup>
+                  <Label for="Password">Password</Label>
+                  <Input
+                    value={values.password}
+                    id="Password"
+                    name="password"
+                    placeholder="password"
+                    type="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.password && touched.password && (
+                    <p className="text-danger">{errors.password}</p>
+                  )}
+                </FormGroup>
+                <div className="mb-3 d-flex justify-content-between">
+                  <FormGroup check>
+                    <Input type="checkbox" />
+                    <Label check>Remember Me</Label>
+                  </FormGroup>
+                  <a href="#" className="link" onClick={props.onForgotPwd}>
+                    Forgot password?
+                  </a>
+                </div>
+                <Button className="w-100 mb-3 btn-dark" type="submit">
+                  Login
+                </Button>
+
+                <div className="d-flex align-items-center gap-3 justify-content-center">
+                  <span>Create a new Account: </span>
+                  <Button className="btn-warning" onClick={props.onSignUp}>
+                    SignUp
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/container/Login/LoginPage.js
+++ src/container/Login/LoginPage.js
@@ -6,89 +6,88 @@ import Login from "./Login";
 import SingUp from "./SignUp";
 
 function LoginPage(props) {
-    const [userType, setUserType] = useState("login");
-    const [forgotPassword, setForgotPassword] = useState(false);
+  const [userType, setUserType] = useState("login");
+  const [forgotPassword, setForgotPassword] = useState(false);
 
-    const signUpHandler = () => {
-        setUserType("signup");
+  const signUpHandler = () => {
+    setUserType("signup");
+  };
+  const loginHandler = () => {
+    setUserType("login");
+    setForgotPassword(false);
+  };
+  const forgotPasswordHandler = () => {
+    setForgotPassword(true);
+    setUserType("forgotPassword");
+  };
+
+  let schemaObj;
+
+  if (userType === "login") {
+    schemaObj = {
+      email: yup
+        .string()
+        .email("Please enter valid Email.")
+        .required("Required!"),
+      password: yup
+        .string()
+        .min(4, "Password should have minimum 4 characters")
+        .max(8, "Password should have maximum 8 characters")
+        .required("Required!"),
     };
-    const loginHandler = () => {
-        setUserType("login");
-        setForgotPassword(false);
+  } else if (userType === "signup") {
+    schemaObj = {
+      name: yup.string().max(15).required("Required!"),
+      email: yup
+        .string()
+        .email("Please enter valid Email.")
+        .required("Required!"),
+      password: yup
+        .string()
+        .min(4, "Password should have minimum 4 characters")
+        .required("Required!"),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "password must be match.")
+        .required("Required!"),
     };
-    const forgotPasswordHandler = () => {
-        setForgotPassword(true);
-        setUserType('forgotPassword')
+  } else {
+    schemaObj = {
+      email: yup
+        .string()
+        .email("Please enter valid Email.")
+        .required("Required!"),
     };
+  }
 
-    let schemaObj;
+  const schema = yup.object().shape(schemaObj);
 
-    if (userType === 'login') {
-        schemaObj = {
-            email: yup
-                .string()
-                .email("Please enter valid Email.")
-                .required("Required!"),
-            password: yup
-                .string()
-                .min(4, "Password should have minimum 4 characters")
-                .max(8, "Password should have maximum 8 characters")
-                .required("Required!"),
-        };
-    } else if (userType === 'signup') {
-        schemaObj = {
-            name: yup.string().max(15).required("Required!"),
-            email: yup
-                .string()
-                .email("Please enter valid Email.")
-                .required("Required!"),
-            password: yup
-                .string()
-                .min(4, "Password should have minimum 4 characters")
-                .max(8, "Password should have maximum 8 characters")
-                .required("Required!"),
-            confirmPassword: yup
-                .string()
-                .oneOf([yup.ref("password"), null], "password must be match.")
-                .required("Required!"),
-        };
-    } else {
-        schemaObj = {
-            email: yup
-                .string()
-                .email("Please enter valid Email.")
-                .required("Required!"),
-        }
-    }
-
-    const schema = yup
-        .object()
-        .shape(schemaObj);
-
-    const formContent =
-        userType === "login" ? (
-            <Login
-                onSignUp={signUpHandler}
-                onForgotPwd={forgotPasswordHandler}
-                schema={schema}
-            />
-        ) : (
-            <SingUp onLogin={loginHandler} schema={schema} />
-        );
-    return (
-        <section>
-            <div className="container">
-                <div className="section-title">
-                    {!forgotPassword && (
-                        <h2>{userType === "login" ? "Login" : "SignUp"}</h2>
-                    )}
-                    {forgotPassword && <h2>Forgot Password</h2>}
-                </div>
-            </div>
-            {!forgotPassword && formContent}
-            {forgotPassword && <ForgotPassword schema={schema} onBack={loginHandler} />}
-        </section>
+  const formContent =
+    userType === "login" ? (
+      <Login
+        onSignUp={signUpHandler}
+        onForgotPwd={forgotPasswordHandler}
+        schema={schema}
+      />
+    ) : (
+      <SingUp onLogin={loginHandler} schema={schema} />
     );
+  return (
+    <section>
+      <div className="container">
+        <div className="section-title">
+          {!forgotPassword && (
+            <h2>{userType === "login" ? "Login" : "SignUp"}</h2>
+          )}
+          {forgotPassword && <h2>Forgot Password</h2>}
+        </div>
+      </div>
+      {!forgotPassword && formContent}
+      {forgotPassword && (
+        <ForgotPassword schema={schema} onBack={loginHandler} />
+      )}
+    </section>
+  );
 }
 
 export default LoginPage;
