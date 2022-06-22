a695fbc00f1d40920501437a0312e106558fe1a2
Bhargav1112 <bhargavbhakhar1112@gmail.com>
2022-06-22 08:59:47 +0530

    forms

--- src/container/Departments/Contact.js
+++ src/container/Departments/Contact.js
@@ -1,68 +1,151 @@
-import React from 'react';
+import React from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
 
 function Contact(props) {
-    return (
-        <main id="contact">
-            <section id="contact" className="contact">
-                <div className="container">
-                    <div className="section-title">
-                        <h2>Contact</h2>
-                        <p>Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc aliquam eget nibh eu euismod. Donec dapibus
-                            blandit quam volutpat sollicitudin. Aenean ac turpis ante. Mauris velit sapien, aliquet aliquet rhoncus
-                            quis,
-                            luctus at neque. Mauris sit amet massa sed orci vehicula facilisis.</p>
-                    </div>
+  const schema = yup.object().shape({
+    name: yup.string().required("Required"),
+    email: yup.string().email("Please enter valid email").required("Required"),
+    subject: yup.string().required("Required"),
+    message: yup.string().required("Required"),
+  });
+
+  const { handleBlur, handleChange, handleSubmit, values, touched, errors } =
+    useFormik({
+      initialValues: {
+        email: "",
+        name: "",
+        subject: "",
+        message: "",
+      },
+      validationSchema: schema,
+      onSubmit: (values, action) => {
+        alert(JSON.stringify(values));
+        action.resetForm();
+      },
+    });
+
+  return (
+    <main id="contact">
+      <section id="contact" className="contact">
+        <div className="container">
+          <div className="section-title">
+            <h2>Contact</h2>
+            <p>
+              Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc
+              aliquam eget nibh eu euismod. Donec dapibus blandit quam volutpat
+              sollicitudin. Aenean ac turpis ante. Mauris velit sapien, aliquet
+              aliquet rhoncus quis, luctus at neque. Mauris sit amet massa sed
+              orci vehicula facilisis.
+            </p>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-lg-4">
+              <div className="info">
+                <div className="address">
+                  <i className="bi bi-geo-alt" />
+                  <h4>Location:</h4>
+                  <p> F-505, Inovative Plazza New Delhi, India</p>
                 </div>
-                <div className="container">
-                    <div className="row mt-5">
-                        <div className="col-lg-4">
-                            <div className="info">
-                                <div className="address">
-                                    <i className="bi bi-geo-alt" />
-                                    <h4>Location:</h4>
-                                    <p> F-505, Inovative Plazza New Delhi, India</p>
-                                </div>
-                                <div className="email">
-                                    <i className="bi bi-envelope" />
-                                    <h4>Email:</h4>
-                                    <p>cityhospital@example.com</p>
-                                </div>
-                                <div className="phone">
-                                    <i className="bi bi-phone" />
-                                    <h4>Call:</h4>
-                                    <p>+91 9988776655</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-8 mt-5 mt-lg-0">
-                            <form action method="post" role="form" className="php-email-form">
-                                <div className="row">
-                                    <div className="col-md-6 form-group">
-                                        <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
-                                    </div>
-                                    <div className="col-md-6 form-group mt-3 mt-md-0">
-                                        <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
-                                    </div>
-                                </div>
-                                <div className="form-group mt-3">
-                                    <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
-                                </div>
-                                <div className="form-group mt-3">
-                                    <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
-                                </div>
-                                <div className="my-3">
-                                    <div className="loading">Loading</div>
-                                    <div className="error-message" />
-                                    <div className="sent-message">Your message has been sent. Thank you!</div>
-                                </div>
-                                <div className="text-center"><button type="submit">Send Message</button></div>
-                            </form>
-                        </div>
-                    </div>
+                <div className="email">
+                  <i className="bi bi-envelope" />
+                  <h4>Email:</h4>
+                  <p>cityhospital@example.com</p>
                 </div>
-            </section>
-        </main>
-    );
+                <div className="phone">
+                  <i className="bi bi-phone" />
+                  <h4>Call:</h4>
+                  <p>+91 9988776655</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-8 mt-5 mt-lg-0">
+              <form
+                method="post"
+                role="form"
+                className="php-email-form"
+                onSubmit={handleSubmit}
+              >
+                <div className="row">
+                  <div className="col-md-6 form-group">
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control"
+                      id="name"
+                      placeholder="Your Name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}
+                    />
+                    {errors.name && touched.name && (
+                      <p className="text-danger">{errors.name}</p>
+                    )}
+                  </div>
+                  <div className="col-md-6 form-group mt-3 mt-md-0">
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      id="email"
+                      placeholder="Your Email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                    />
+                    {errors.email && touched.email && (
+                      <p className="text-danger">{errors.email}</p>
+                    )}
+                  </div>
+                </div>
+                <div className="form-group mt-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="subject"
+                    id="subject"
+                    placeholder="Subject"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.subject}
+                  />
+                  {errors.subject && touched.subject && (
+                    <p className="text-danger">{errors.subject}</p>
+                  )}
+                </div>
+                <div className="form-group mt-3">
+                  <textarea
+                    className="form-control"
+                    name="message"
+                    rows={5}
+                    placeholder="Message"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.message}
+                  />
+                  {errors.message && touched.message && (
+                    <p className="text-danger">{errors.message}</p>
+                  )}
+                </div>
+                <div className="my-3">
+                  <div className="loading">Loading</div>
+                  <div className="error-message" />
+                  <div className="sent-message">
+                    Your message has been sent. Thank you!
+                  </div>
+                </div>
+                <div className="text-center">
+                  <button type="submit">Send Message</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Contact;
