0f01e7e986cb23d197c2e1e8a653729bf4602235
Bhakhar Bhargav <bhargavbhakhar1112@gmail.com>
2022-07-06 19:25:35 +0530

    editing done

--- src/container/Departments/Apointments.js
+++ src/container/Departments/Apointments.js
@@ -4,31 +4,41 @@ import BookApointment from "../Apointment/BookApointment";
 import ListApointment from "../Apointment/ListApointment";
 
 function Apointments(props) {
-  return (
-    <main id="make_appointment">
-      <section id="appointment" className="appointment">
-        <div className="container">
-          <div className="section-title">
-            <h2>Manage Appointments</h2>
-          </div>
-          <div className="row">
-            <div className="col-6">
-              <NavLink to={`/apointment/book`}>Book Apointment</NavLink>
-            </div>
-            <div className="col-6">
-              <NavLink to="/apointment/list">Apointments</NavLink>
-            </div>
-          </div>
-        </div>
-        <Route path="/apointment/book">
-          <BookApointment />
-        </Route>
-        <Route path="/apointment/list">
-          <ListApointment />
-        </Route>
-      </section>
-    </main>
-  );
+    return (
+        <main id="make_appointment">
+            <section id="appointment" className="appointment">
+                <div className="container">
+                    <div className="section-title">
+                        <h2>Manage Appointments</h2>
+                    </div>
+                    <div className="row">
+                        <div className="col-6 text-center">
+                            <NavLink
+                                to={`/apointment/book`}
+                                className="appointment-btn"
+                            >
+                                Book Apointment
+                            </NavLink>
+                        </div>
+                        <div className="col-6 text-center">
+                            <NavLink
+                                to="/apointment/list"
+                                className="appointment-btn"
+                            >
+                                Apointments
+                            </NavLink>
+                        </div>
+                    </div>
+                </div>
+                <Route path="/apointment/book">
+                    <BookApointment />
+                </Route>
+                <Route path="/apointment/list">
+                    <ListApointment />
+                </Route>
+            </section>
+        </main>
+    );
 }
 
 export default Apointments;
