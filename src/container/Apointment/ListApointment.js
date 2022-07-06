e80410f0d2db86b0e73c18d109317a9192a36969
Bhargav1112 <bhargavbhakhar1112@gmail.com>
2022-07-06 15:46:45 +0530

    crud

--- src/container/Apointment/ListApointment.js
+++ src/container/Apointment/ListApointment.js
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+} from "reactstrap";
 
 function ListApointment(props) {
-    return (
-        <div>
-            <h1>List apointment</h1>
-        </div>
-    );
+  const [data, setData] = useState([]);
+  const history = useHistory();
+
+  const getData = () => {
+    const localData = JSON.parse(localStorage.getItem("apt"));
+    if (!localData) return;
+    setData(localData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const deleteHandler = (id) => {
+    const localData = JSON.parse(localStorage.getItem("apt"));
+    const remainingData = localData.filter((apt) => apt.id !== id);
+    localStorage.setItem("apt", JSON.stringify(remainingData));
+    setData(remainingData);
+  };
+
+  const editHandler = (id) => {
+    history.push("/apointment/book", { id });
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center my-5">All Apointments</h1>
+      {data.map((d) => {
+        return (
+          <Card key={d.id}>
+            <CardBody>
+              <CardTitle tag="h5">{d.name}</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                {d.email}
+              </CardSubtitle>
+              <CardText>{d.date}</CardText>
+              <CardText>Mob. : {d.phone}</CardText>
+              <CardText>Dept. : {d.select}</CardText>
+              <CardText>Message : {d.message}</CardText>
+              <Button onClick={editHandler.bind(null, d.id)} className="me-3">
+                Edit
+              </Button>
+              <Button onClick={deleteHandler.bind(null, d.id)}>Delete</Button>
+            </CardBody>
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ListApointment;
--- src/container/Departments/Apointments.js
+++ src/container/Departments/Apointments.js
@@ -4,33 +4,31 @@ import BookApointment from "../Apointment/BookApointment";
 import ListApointment from "../Apointment/ListApointment";
 
 function Apointments(props) {
-    return (
-        <main id="make_appointment">
-            <section id="appointment" className="appointment">
-                <div className="container">
-                    <div className="section-title">
-                        <h2>Manage Appointments</h2>
-                    </div>
-                    <div className="row">
-                        <div className="col-6">
-                            <NavLink to={`/apointment/book`}>
-                                Book Apointment
-                            </NavLink>
-                        </div>
-                        <div className="col-6">
-                            <NavLink to="/apointment/list">Apointments</NavLink>
-                        </div>
-                    </div>
-                </div>
-                <Route path="/apointment/book">
-                    <BookApointment />
-                </Route>
-                <Route path="/apointment/list">
-                    <ListApointment />
-                </Route>
-            </section>
-        </main>
-    );
+  return (
+    <main id="make_appointment">
+      <section id="appointment" className="appointment">
+        <div className="container">
+          <div className="section-title">
+            <h2>Manage Appointments</h2>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <NavLink to={`/apointment/book`}>Book Apointment</NavLink>
+            </div>
+            <div className="col-6">
+              <NavLink to="/apointment/list">Apointments</NavLink>
+            </div>
+          </div>
+        </div>
+        <Route path="/apointment/book">
+          <BookApointment />
+        </Route>
+        <Route path="/apointment/list">
+          <ListApointment />
+        </Route>
+      </section>
+    </main>
+  );
 }
 
 export default Apointments;
